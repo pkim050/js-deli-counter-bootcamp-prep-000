c05690a724a656b5d473befdb1d12ddf3ed3b8b7
Patrick A Kim <pkim050@gmail.com>
2018-11-08 22:17:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine, name) {
   var length = katzDeliLine.length;
   for (var i = 0; i < length; i++) {
-    if (i === length - 1) {
+    if (i === (length - 1)) {
       var temp = katzDeliLine[i];
       return `Welcome, ${name}. You are number ${temp} in line.`;
     }
